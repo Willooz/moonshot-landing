5118cee072ee19d0b355ef197f0e52b17ae6067c
Willem <willem.browne@gmail.com>
2015-07-28 16:44:46 -0400

    added contact model to store form inputs, contacts controller, routes and thanks pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!, unless: :pages_controller?
-
   # Uncomment these lines to get pundit
   # include Pundit
   # after_action :verify_authorized, except:  :index, unless: :devise_or_pages_or_admin_controller?
@@ -12,16 +10,16 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def devise_or_pages_or_admin_controller?
-    devise_controller? || pages_controller? || params[:controller] =~ /^admin/
-  end
+  # def devise_or_pages_or_admin_controller?
+  #   devise_controller? || pages_controller? || params[:controller] =~ /^admin/
+  # end
 
   def pages_controller?
     controller_name == "pages"  # Brought by the `high_voltage` gem
   end
 
-  def user_not_authorized
-    flash[:error] = I18n.t('controllers.application.user_not_authorized', default: "You can't access this page.")
-    redirect_to(root_path)
-  end
+  # def user_not_authorized
+  #   flash[:error] = I18n.t('controllers.application.user_not_authorized', default: "You can't access this page.")
+  #   redirect_to(root_path)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+
+  get '/', to: 'contacts#new', as: 'home'
+  get '/thanks', to: 'contacts#thanks', as: 'thanks'
+  post '/contacts', to: 'contacts#create', as: 'contacts'
+
 end
